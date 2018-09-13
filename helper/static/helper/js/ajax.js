5a70d41be5acce34334e3be2ef3f123761cf6769
Samuel Magiera <samuel@smagiera.pl>
2018-09-13 16:17:17 +0200

    table translation on policy list page

--- helper/static/helper/js/ajax.js
+++ helper/static/helper/js/ajax.js
@@ -2,7 +2,10 @@ function update() {
   var date1 = document.getElementById("from").value;
   var date2 = document.getElementById("to").value;
   $('#results').load("policies/"+date1+"&"+date2+" #result", function() {
-    var table = $('#policy_list').DataTable();
+    var table = $('#policy_list').DataTable({
+      language: {
+          url: 'https://cdn.datatables.net/plug-ins/1.10.19/i18n/Polish.json'
+      }});
     new $.fn.dataTable.Buttons(table, {
 buttons: [
     'excel', 'pdf'
