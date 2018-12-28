ea1cbb2622fad8a383bb77bcaf7ac0326943a6e0
Samuel Magiera <samuel@smagiera.pl>
2018-12-28 15:39:53 +0100

    remove debug

--- agenthelper/settings.py
+++ agenthelper/settings.py
@@ -34,12 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
-if LIVEHOST:
-    DEBUG = False
-else:
-    DEBUG = True
 
-#ALLOWED_HOSTS = ['192.168.1.50', '127.0.0.1']
 ALLOWED_HOSTS = ['agenthelper.smagiera.pl']
 
 # Application definition
